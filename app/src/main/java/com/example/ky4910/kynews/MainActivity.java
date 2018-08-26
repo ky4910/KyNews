e93b69eb7538537507a7997c455a96653defdba2
ky4910 <kobe4910@qq.com>
2018-08-26 22:35:39 +0800

    create the project for KyNews

---
+++ app/src/main/java/com/example/ky4910/kynews/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.ky4910.kynews;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
