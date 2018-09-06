b27bc701e766d288eadcc1e3c31bd5917a6cddd7
ky4910 <kobe4910@qq.com>
2018-09-06 22:47:08 +0800

    Disable actionbar add achieve toolbar+searchview

--- app/src/main/java/com/example/ky4910/kynews/MainActivity.java
+++ app/src/main/java/com/example/ky4910/kynews/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.ky4910.kynews;
 
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    SearchView mSearchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolBar = (Toolbar)findViewById(R.id.tool_Bar);
+        toolBar.setTitle("极速新闻");
+        setSupportActionBar(toolBar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar, menu);
+        MenuItem searchItem = menu.findItem(R.id.search);
+        mSearchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+        return super.onCreateOptionsMenu(menu);
     }
 }
