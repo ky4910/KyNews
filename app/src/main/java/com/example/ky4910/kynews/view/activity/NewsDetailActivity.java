24bb2ec5e7a98348bc25eda6d913e0031b2414e4
Kimber Jin <KimberJin@viatech.com.cn>
2019-06-10 14:38:26 +0800

    found solution to load mobile version news details

--- app/src/main/java/com/example/ky4910/kynews/view/activity/NewsDetailActivity.java
+++ app/src/main/java/com/example/ky4910/kynews/view/activity/NewsDetailActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 
 import com.example.ky4910.kynews.R;
@@ -12,6 +13,10 @@ public class NewsDetailActivity extends AppCompatActivity {
 
     private WebView mWebView;
 
+    // http://news.163.com/19/0610/12/EHAKCCGO0001899N.html
+    // 重定向为 =》 http://3g.163.com/news/article/EHAKCCGO0001899N.html
+    // need fix webview cannot load image issue
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +24,7 @@ public class NewsDetailActivity extends AppCompatActivity {
         Intent intent = getIntent();
         String newsUrl = intent.getStringExtra("detail_news");
         mWebView = (WebView)findViewById(R.id.web_view);
-        mWebView.loadUrl(newsUrl);
+        //mWebView.loadUrl(newsUrl);
+        mWebView.loadUrl("http://3g.163.com/news/article/EHAKCCGO0001899N.html");
     }
 }
