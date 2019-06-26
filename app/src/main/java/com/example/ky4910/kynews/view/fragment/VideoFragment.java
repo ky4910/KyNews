b14f0f030c5ca705bb8b71f8e64084bcc41faec0
ky4910 <kobe4910@qq.com>
2019-06-26 22:05:46 +0800

    init video recyclerview(no VideoPlayerView)

--- app/src/main/java/com/example/ky4910/kynews/view/fragment/VideoFragment.java
+++ app/src/main/java/com/example/ky4910/kynews/view/fragment/VideoFragment.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -63,7 +66,14 @@ public class VideoFragment extends Fragment {
                     " videoTitle: " + trailer.getVideoTitle());
         }
         recyclerView = view.findViewById(R.id.video_rv);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity(),
+                LinearLayoutManager.VERTICAL, false));
+        recyclerView.addItemDecoration(new DividerItemDecoration(getActivity(),
+                DividerItemDecoration.VERTICAL));
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+
         videoAdapter = new VideoRvAdapter(getContext(), trailersBeanList);
+        recyclerView.setAdapter(videoAdapter);
     }
 
     public String convertStreamToString(InputStream inputStream) {
